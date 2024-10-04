2a73a484e7a3a5e0d41031a90597b1267e40d6ac
CristianTambaco <168015193+CristianTambaco@users.noreply.github.com>
2024-10-04 13:03:43 -0500

    Add files via upload

---
+++ PRACTICA1/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("NOMBRE: Cristian Tambaco");
+        System.out.println("DIRECCION: Joaquin Mancheno y Calle B");
+        System.out.println("TELEFONO: 2480523");
+
+    }
+}
